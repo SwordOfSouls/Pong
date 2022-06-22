3768846c11ed321c095373be73458c84e1d257ff
SwordOfSouls <atolesnikov@gmail.com>
2022-06-22 15:25:48 -0700

    Add project files.

---
+++ Pong/Game1.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private Sprite ball;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ball = new Sprite(this,spriteBatch, "ball", new Vector2(75, 75),.4f);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkCyan);
+
+            spriteBatch.Begin();
+            ball.Draw();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Pong/Sprite.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Pong
+{
+    public class Sprite
+    {
+        readonly SpriteBatch batch;
+
+        readonly Texture2D texture;
+        readonly Vector2 origin;
+
+        Vector2 position = new Vector2(0,0);
+        Color tint = Color.White;
+        SpriteEffects effects = SpriteEffects.None;
+
+        int rotation = 0;
+        float scale;
+
+        public Sprite(Game game,SpriteBatch batch, string texture, Vector2 origin, float scale)
+        {
+            this.batch = batch;
+            this.texture = game.Content.Load<Texture2D>(texture);
+            this.origin = origin;
+            this.scale = scale;
+        }
+
+        public void Draw()
+        {
+            batch.Draw(texture, position, null, tint, rotation, origin,scale, effects, 0);
+        }
+
+        public void SetPosition(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public void SetTint(Color tint)
+        {
+            this.tint = tint;
+        }
+
+        public void setEffects(SpriteEffects effects)
+        {
+            this.effects = effects;
+        }
+
+        public void setRotation(int rotation)
+        {
+            this.rotation = rotation;
+        }
+
+        public void setScale(float scale)
+        {
+            this.scale = scale;
+        }
+    }
+}
